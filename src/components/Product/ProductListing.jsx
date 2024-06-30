20aa7c127ce9a2cba692366c493a7b64840ba9b4
SQLAdmin <abuyeahia24@gmail.com>
2024-06-30 15:42:05 +0600

    redirect added

--- src/components/Product/ProductListing.jsx
+++ src/components/Product/ProductListing.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { fetchProducts } from '../../serveices/productServices';
-import { FaStar } from 'react-icons/fa';
+// import { FaStar } from 'react-icons/fa';
 
 const ProductListing = () => {
     const [products, setProducts] = useState([]);
@@ -61,10 +61,11 @@ const ProductListing = () => {
     };
 
 
-    // pagination part
+    
     const indexOfLastProduct = currentPage * productsPerPage;
     const indexOfFirstProduct = indexOfLastProduct - productsPerPage;
     const currentProducts = filteredProducts.slice(indexOfFirstProduct, indexOfLastProduct);
+
 
     // loader
     if (loading) {
@@ -119,22 +120,27 @@ const ProductListing = () => {
                     <button onClick={() => handleSort('asc')} className='px-4 py-2 border border-gray-300 rounded-md text-gray-700 hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-gray-400'> Low to High</button>
                     <button onClick={() => handleSort('desc')} className='px-4 py-2 border border-gray-300 rounded-md text-gray-700 hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-gray-400'>High to Low</button>
                 </div>
+
+                
                 <div className="container mx-auto px-4">
                     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
                         {currentProducts.map(product => (
                             <div key={product.id} className="bg-white rounded-lg shadow-md overflow-hidden">
-                                <img src={product.image} alt={product.title} className="w-full h-48 object-cover hover:scale-110 transition duration-500 cursor-pointer" />
+                                <img
+                                    src={product.image}
+                                    alt={product.title}
+                                    className="w-full h-48 object-cover hover:scale-110 transition duration-500 cursor-pointer"
+                                />
                                 <div className="p-4">
                                     <h3 className="text-lg font-semibold">{product.title}</h3>
-                                    <div className='flex justify-between my-2'>
-                                    <p className="text-gray-700">${product.price}</p>
-                                    <p className="text-gray-700">Rating {product.rating.rate}</p>
+                                    <div className="flex justify-between items-center my-2">
+                                        <p className="text-gray-700 text-sm lg:text-base">${product.price}</p>
+                                        <p className="text-gray-700 text-sm lg:text-base">Rating {product.rating.rate}</p>
                                     </div>
-
                                 </div>
-
                             </div>
                         ))}
+
                     </div>
                 </div>
 
--- src/serveices/productServices.js
+++ src/serveices/productServices.js
@@ -5,6 +5,8 @@ const API = 'https://fakestoreapi.com/products';
 export const fetchProducts = async () => {
     try {
         const response = await axios.get(API);
+        
+        console.log(response.data);
         return response.data;
     } catch (error) {
         console.log('error fetching products:', error);
